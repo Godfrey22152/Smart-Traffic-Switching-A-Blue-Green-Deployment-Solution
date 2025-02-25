e9b1733925994d999bb5c5f0cc5a140fc1c871b5
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2025-02-25 22:33:18 +0100

    Updated create_jenkins_rbac.sh

--- Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
+++ Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
@@ -2,6 +2,12 @@
 
 # Define namespace
 NAMESPACE="webapps"
+ROLE="app-role"
+ROLEBINDING="app-rolebinding"
+SECRET="jenkins-secret"
+SERVICEACCOUNT="jenkins"
+RBAC_CONFIG_FILE="jenkins_rbac_config.yaml"
+
 
 # Check if namespace exists, if not, create it
 kubectl get namespace $NAMESPACE >/dev/null 2>&1
@@ -12,24 +18,24 @@ else
   echo "Namespace $NAMESPACE already exists."
 fi
 
-# Create the resources using the jenkins_rbac_config.yaml file
-echo "Creating Jenkins ServiceAccount, Role, RoleBinding, and Secret..."
-kubectl apply -f jenkins_rbac_config.yaml
+# Create the resources using the github-actions_rbac_config.yaml file
+echo "Creating Github Actions ServiceAccount, Role, RoleBinding, and Secret..."
+kubectl apply -f $RBAC_CONFIG_FILE
 
 # Verify the creation of the ServiceAccount, Role, and RoleBinding
-kubectl get serviceaccount jenkins -n $NAMESPACE
-kubectl get role app-role -n $NAMESPACE
-kubectl get rolebinding app-rolebinding -n $NAMESPACE
+kubectl get serviceaccount $SERVICEACCOUNT -n $NAMESPACE
+kubectl get role $ROLE -n $NAMESPACE
+kubectl get rolebinding $ROLEBINDING -n $NAMESPACE
 
 # Wait for the secret to be created
-echo "Waiting for the secret to be created..."
+echo "Waiting for the $SECRET secret to be created..."
 sleep 5
 
 # Get the secret for the service account and display it
-SECRET_NAME=$(kubectl get secret -n $NAMESPACE | grep jenkins-secret | awk '{print $1}')
+SECRET_NAME=$(kubectl get secret -n $NAMESPACE | grep $SECRET | awk '{print $1}')
 
 if [ -z "$SECRET_NAME" ]; then
-  echo "Error: Could not find the secret for the ServiceAccount jenkins."
+  echo "Error: Could not find the $SECRET secret for the ServiceAccount $SERVICEACCOUNT."
 else
   echo "ServiceAccount secret created: $SECRET_NAME"
 
@@ -39,9 +45,9 @@ else
   if [ -z "$SECRET_TOKEN" ]; then
     echo "Error: Could not retrieve the token from the secret."
   else
-    echo "Secret token for the ServiceAccount jenkins:"
+    echo "Secret token for the ServiceAccount $SERVICEACCOUNT:"
     echo "$SECRET_TOKEN"
   fi
 fi
 
-echo "Jenkins service account and RBAC resources created successfully!"
+echo "$SERVICEACCOUNT service account and RBAC resources created successfully!"
